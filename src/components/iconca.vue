efdf6a2795bba2962f08ec345bc72204928530de
unknown <damir777xolnazarov@gmail.com>
2023-11-02 23:00:49 +0500

    first commit

---
+++ src/components/iconca.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="icon">
+        <img src="../assets/logo_pixian_ai 1.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<script setup>
+import icon from '../components/iconca.vue'
+import loading from '../components/loadingBar.vue'
+</script>
+
+<template>
+  <div class="home">
+     <icon class="icon"/>
+     <loading v-if="loadi"/>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            loadi: false
+        };
+    },
+
+    created() {
+      setTimeout(() => {
+        this.loadi = true
+      }, 5000);
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+<style scoped>
+@keyframes logo{
+  0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 100%;
+  }
+}
+@media screen and (min-width: 300px) {
+  .home {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-top: 200px;
+    align-items: center;
+    justify-content: space-between;
+    padding-bottom: 150px;
+    background-color: #2a2a72;
+    background-image: linear-gradient(315deg, #2a2a72 0%, #009ffd 74%);
+  }
+  .icon{
+    animation: 2.5s logo ease-out;
+  }
+}
+</style>
